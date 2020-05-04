bdf0778d832db59ede71d719ee9a73898e068d18
ksmith-hub24 <60757618+ksmith-hub24@users.noreply.github.com>
2020-05-04 19:07:26 -0400

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,24 @@
+var images = [
+  'business.jpg',
+  'trust.jpg',
+  'growth.jpg'
+
+];
+
+var num = 0;
+function next() {
+  var slider = document.getElementById('slider');
+  num++;
+  if(num >= images.length){
+    num = 0;
+  }
+  slider.src = images[num];
+}
+function prev() {
+  var slider = document.getElementById('slider');
+  num--;
+  if(num < 0) {
+    num = images.length-1;
+  }
+  slider.src = images[num];
+}
